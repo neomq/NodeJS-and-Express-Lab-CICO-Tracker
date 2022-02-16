74d4f8dfdf640a5f63eab7dbcf96dc235c448f78
neomq <neominqi@hotmail.com>
2022-02-16 07:52:20 +0000

    commit Lab-01

---
+++ Lab-01/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+let app = express();
+
+// add routes here
+app.get('/', function(req,res){
+    res.send("<h1>Hello from Express</h1>");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server started")
+})
+
