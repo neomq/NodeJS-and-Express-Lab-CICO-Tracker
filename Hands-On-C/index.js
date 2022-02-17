3951667964836b0eb77a706ed60330f5582cd983
neomq <neominqi@hotmail.com>
2022-02-17 16:27:40 +0000

    done Hands-On-C

--- Hands-On-C/index.js
+++ Hands-On-C/index.js
@@ -25,23 +25,54 @@ app.get('/lostandfound', (req,res)=>{
     res.render('lostandfound')
 })
 
-// // > process the form
-// app.post('/lostandfound', function(req,res){
-//     let itemName = req.body.name;
-//     let hasError = false;
-//     if (!itemName) {
-//         hasError = true;
-//     }
-//     if (itemName && itemName.length < 3) {
-//         hasError = true;
-//     }
+// > process the form
+app.post('/lostandfound', function(req,res){
+    
+    let hasError = false;
 
-//     let email = req.body.email;
-//     if (email && !email.includes('@')) {
-//         hasError = true;
-//     }
+    // item name
+    let itemName = req.body.name;
 
-// })
+    if (!itemName) {
+        hasError = true;
+    }
+    if (itemName && itemName.length < 3 && itemName.length < 200) {
+        hasError = true;
+    }
+
+    // email
+    let email = req.body.email;
+    if (email && !email.includes('@') && !email.includes('.')) {
+        hasError = true;
+    }
+
+    // location
+    let location = req.body.location;
+    if (!location) {
+        hasError = true;
+    }
+
+    // properties
+    let properties = req.body.properties;
+    if (properties){
+        if (Array.isArray(properties)){
+            if(properties.length > 3){
+                hasError = true;
+            }
+        } else {
+            hasError;
+        }
+    } else {
+        hasError = true;
+    }
+
+    
+    if (hasError) {
+        res.sendStatus(406)
+    } else {
+        res.send("Success")
+    }
+})
 
 // LISTEN
 app.listen(3000, function(){
